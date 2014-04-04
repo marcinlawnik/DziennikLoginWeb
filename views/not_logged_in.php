f6521ce913737c896c8c376fd3473d4d9167c754
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-04-04 20:48:59 +0200

    Update not_logged_in.php

--- views/not_logged_in.php
+++ views/not_logged_in.php
@@ -1,9 +1,9 @@
 <?php include('_header.php'); ?>
     <div class="container">
         <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-            <div class="panel panel-info" >
+            <div class="panel panel-primary" >
                     <div class="panel-heading">
-                        <div class="panel-title">Zaloguj się.</div>
+                        <div class="panel-title">Zaloguj się</div>
                         <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="password_reset.php"><?php echo WORDING_FORGOT_MY_PASSWORD; ?></a></div>
                     </div>
 
