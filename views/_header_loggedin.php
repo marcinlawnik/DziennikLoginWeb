bf4d99d5eeadbc8458f661d7f6279ca35e83399d
Robsonwp <perzfilip@gmail.com>
2014-04-08 21:35:02 +0200

    poprawa obrazka

--- views/_header_loggedin.php
+++ views/_header_loggedin.php
@@ -51,7 +51,7 @@
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" SRC="http://beta.lawniczak.me/logo_small.png"></a>
+    <img class="navbar-brand" SRC="http://beta.lawniczak.me/logo_small.png"></img>
   </div>
 
   <!-- Collect the nav links, forms, and other content for toggling -->
