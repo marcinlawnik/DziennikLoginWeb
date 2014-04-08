48d94bd36414b7d8fbc96c63a6bb7d9574fc7eb2
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-04-08 22:09:43 +0200

    Update test.php

--- views/test.php
+++ views/test.php
@@ -1,158 +1,97 @@
 <?php include('_header_loggedin.php'); ?>
 
+<form action="index.php" method="post">
+<input type="submit" name="wyslij" value="Wyswietl" />
+Zaznacz wszystko:
+<input type="checkbox" name="wyczysc" checked='checked' value="Wyczysc" onclick="var w = document.getElementsByTagName('input'); for(var i = 0; i < w.length; i++){ if(w[i].value=='przedmioty'){ w[i].checked = this.checked;};};"/>
+<?php
+$connection = @mysqli_connect(DB_HOST, 
 
-<div class="container">
-    <div style="align:right;" class="row">
-        <div class="col-sm-3 col-md-3">
-            <div class="panel-group" id="accordion">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne"><span class="glyphicon glyphicon-headphones">
-                            </span>Przedmiot</a>
-                        </h4>
-                    </div>
-                    <div id="collapseOne" class="panel-collapse collapse in">
-                        <div class="panel-body">
-                            <table class="table">
-                                <tr>
-                                    <td>
-                                        <span class="glyphicon glyphicon-headphones text-primary"></span><a href="http://www.jquery2dotnet.com">Articles</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <span class="glyphicon glyphicon-flash text-success"></span><a href="http://www.jquery2dotnet.com">News</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <span class="glyphicon glyphicon-file text-info"></span><a href="http://www.jquery2dotnet.com">Newsletters</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <span class="glyphicon glyphicon-home"></span><a href="http://www.jquery2dotnet.com">Comments</a>
-                                        <span class="badge">42</span>
-                                    </td>
-                                </tr>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo"><span class="glyphicon glyphicon-trash">
-                            </span>Modules</a>
-                        </h4>
-                    </div>
-                    <div id="collapseTwo" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            <table class="table">
-                                <tr>
-                                    <td>
-                                        <a href="http://www.jquery2dotnet.com">Orders</a> <span class="label label-success">$ 320</span>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <a href="http://www.jquery2dotnet.com">Invoices</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <a href="http://www.jquery2dotnet.com">Shipments</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <a href="http://www.jquery2dotnet.com">Tex</a>
-                                    </td>
-                                </tr>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree"><span class="glyphicon glyphicon-user">
-                            </span>Account</a>
-                        </h4>
-                    </div>
-                    <div id="collapseThree" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            <table class="table">
-                                <tr>
-                                    <td>
-                                        <a href="http://www.jquery2dotnet.com">Change Password</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <a href="http://www.jquery2dotnet.com">Notifications</a> <span class="label label-info">5</span>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <a href="http://www.jquery2dotnet.com">Import/Export</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <span class="glyphicon glyphicon-trash text-danger"></span><a href="http://www.jquery2dotnet.com" class="text-danger">
-                                            Delete Account</a>
-                                    </td>
-                                </tr>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour"><span class="glyphicon glyphicon-file">
-                            </span>Reports</a>
-                        </h4>
-                    </div>
-                    <div id="collapseFour" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            <table class="table">
-                                <tr>
-                                    <td>
-                                        <span class="glyphicon glyphicon-usd"></span><a href="http://www.jquery2dotnet.com">Sales</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <span class="glyphicon glyphicon-user"></span><a href="http://www.jquery2dotnet.com">Customers</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <span class="glyphicon glyphicon-tasks"></span><a href="http://www.jquery2dotnet.com">Products</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <span class="glyphicon glyphicon-shopping-cart"></span><a href="http://www.jquery2dotnet.com">Shopping Cart</a>
-                                    </td>
-                                </tr>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-9 col-md-9">
-            <div class="well">
-                <h1>
-                    Accordion Menu With Icon</h1>
-                Admin Dashboard Accordion Menu
-            </div>
-        </div>
-    </div>
-</div>
+DB_USER, DB_PASS, DB_NAME) 
+or die('Nie dziala'.mysql_error()); 
 
-<?php include('_footer_loggedin.php'); ?>
+$przedmioty = array();
+$skroty = array();
+$sb = mysqli_query($connection, "SELECT * FROM subjects");
+$sbct = mysqli_query($connection, "SELECT count(*) FROM subjects");
+$subjcount = $sbct -> fetch_row();
+while($rek = mysqli_fetch_array($sb)) {
+$przedmioty[$rek[subjectId]] = $rek[subjectName];
+}
+for($i=1; $i<=$subjcount[0]; $i++) {
+echo "<input type='checkbox' checked='checked' name='".$i."' 
+
+value='przedmioty'/>".$przedmioty[$i];
+}
+
+echo "<hr>";
+
+$sb = mysqli_query($connection, "SELECT * FROM grades ORDER BY gradeAbbrev ASC");
+$sk = 0;
+$ostatni_skrot="";
+while($rek = mysqli_fetch_array($sb)) {
+if($ostatni_skrot != $rek[gradeAbbrev]) {
+$skroty[$sk] = $rek[gradeAbbrev];
+$ostatni_skrot = $rek[gradeAbbrev];
+$sk++;
+}
+}
+for($i=1; $i<$sk; $i++) {
+echo "<input type='checkbox' checked='checked' name='".$skroty[$i]."' value='skroty'/>".$skroty[$i];
+}
+
+
+
+if (isset($_POST['wyslij'])){
+
+
+echo "<table border='5' cellpadding='10' >";
+
+for($subj = 1; $subj<31; $subj++) {
+$gr = mysqli_query($connection, "SELECT * FROM grades WHERE userId='".$_SESSION['user_id']."' AND subjectId='".$subj."'");
+
+if(isset($_POST[$subj])) {
+
+echo "<tr bgcolor='#33cc66' align='center'><td height='86' rowspan='2' bgcolor='#ccffff'>".$przedmioty[$subj]."</td>";
+$i=0;
+while ($i<27) {
+$rek = mysqli_fetch_array($gr);
+echo "<td width='20' height='20'>";
+if(isset($_POST[$rek['gradeAbbrev']])) {
+echo $rek['gradeValue'];
+}
+
+echo "</td>";
+$i++;
+
+}
+$i=0;
+echo "</tr>";
+echo "<tr bgcolor='#5555ff' align='center'>";
+$gr = mysqli_query($connection, "SELECT * FROM grades WHERE 
+
+userId='2' AND subjectId='".$subj."'");
+while ($i<27) {
+echo "<td width='20' height='20'>";
+$rek = mysqli_fetch_array($gr);
+if (isset($_POST[$rek['gradeAbbrev']])) {
+echo $rek['gradeWeight'];
+}
+echo "</td>";
+$i++;
+}
+echo "</tr>";
+}
+}
+echo "</table>";
+}
+
+
+
+?>
+
+
+</form>
+
+
+<?php include('_footer_loggedin.php'); ?>\
