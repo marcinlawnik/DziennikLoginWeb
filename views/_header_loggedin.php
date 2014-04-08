221faae62f167134b2030b07a70049d944df36f1
Robsonwp <perzfilip@gmail.com>
2014-04-08 21:23:01 +0200

    usunięcie class

--- views/_header_loggedin.php
+++ views/_header_loggedin.php
@@ -57,7 +57,7 @@
   <!-- Collect the nav links, forms, and other content for toggling -->
   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
     <ul class="nav navbar-nav">
-      <li class="active"><a href="index.php">Strona Główna</a></li>
+      <li><a href="index.php">Strona Główna</a></li>
       <li><a href="grades.php">Oceny</a></li>
       <li class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
