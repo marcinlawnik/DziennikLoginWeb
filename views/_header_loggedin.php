89adbf826b269563d851ba52c7a8384a8d6309c4
Robsonwp <perzfilip@gmail.com>
2014-04-08 17:33:30 +0200

    kreska, wyszukiwarka delate

--- views/_header_loggedin.php
+++ views/_header_loggedin.php
@@ -72,18 +72,10 @@
         </ul>
       </li>
     </ul>
-    <div class="col-sm-3 col-md-3">
-        <form class="navbar-form" role="search">
-        <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search" name="q">
-            <div class="input-group-btn">
-                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-            </div>
-        </div>
-        </form>
-    </div>
+    
     <ul class="nav navbar-nav navbar-right">
-      <li><p><?php echo  $_SESSION['user_name'];?></p></li>
+     <li class="divider"></li>
+      <li><?php echo  $_SESSION['user_name'];?></li>
       
     </ul>
   </div><!-- /.navbar-collapse -->
