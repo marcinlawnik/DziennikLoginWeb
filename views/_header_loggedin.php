dcf1a8122c8b895115d8f661fed3d52558826d16
Robsonwp <perzfilip@gmail.com>
2014-04-08 22:03:54 +0200

    Update _header_loggedin.php

--- views/_header_loggedin.php
+++ views/_header_loggedin.php
@@ -58,6 +58,7 @@
   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
     <ul class="nav navbar-nav">
       <li><a href="index.php">Strona Główna</a></li>
+      <li><a href="test.php">Test</a></li>
       <li><a href="grades.php">Oceny</a></li>
       <li class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
