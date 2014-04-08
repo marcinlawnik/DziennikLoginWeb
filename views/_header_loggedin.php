b005482a58974cb8dfab522001f589e9c6688619
Robsonwp <perzfilip@gmail.com>
2014-04-08 17:30:38 +0200

    grades.php i SG i jquery

--- views/_header_loggedin.php
+++ views/_header_loggedin.php
@@ -36,6 +36,7 @@
         /*]]>*/
     </style>
     <link href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 </head>
 <body>
@@ -56,8 +57,8 @@
   <!-- Collect the nav links, forms, and other content for toggling -->
   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
     <ul class="nav navbar-nav">
-      <li class="active"><a href="index.php">strona główna</a></li>
-      <li><a href="#">Link</a></li>
+      <li class="active"><a href="index.php">Strona Główna</a></li>
+      <li><a href="grades.php">Oceny</a></li>
       <li class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
         <ul class="dropdown-menu">
