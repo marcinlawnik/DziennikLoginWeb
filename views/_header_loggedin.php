124ff000c0eae0952af832812b7360a635a252b2
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-04-08 21:43:42 +0200

    Update _header_loggedin.php

--- views/_header_loggedin.php
+++ views/_header_loggedin.php
@@ -85,3 +85,34 @@
     </ul>
   </div><!-- /.navbar-collapse -->
 </nav>
+<?php
+// show potential errors / feedback (from login object)
+if (isset($login)) {
+    if ($login->errors) {
+        foreach ($login->errors as $error) {
+            echo $error;
+        }
+    }
+    if ($login->messages) {
+        foreach ($login->messages as $message) {
+            echo $message;
+        }
+    }
+}
+?>
+
+<?php
+// show potential errors / feedback (from registration object)
+if (isset($registration)) {
+    if ($registration->errors) {
+        foreach ($registration->errors as $error) {
+            echo $error;
+        }
+    }
+    if ($registration->messages) {
+        foreach ($registration->messages as $message) {
+            echo $message;
+        }
+    }
+}
+?>
