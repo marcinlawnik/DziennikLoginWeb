6cdb7ba5a84c18908d2ea9a4954175deb92bbfb3
Robsonwp <perzfilip@gmail.com>
2014-04-08 20:57:23 +0200

    Usunięcie niepotrzebnego tekstu

--- views/index.php
+++ views/index.php
@@ -3,11 +3,6 @@
 <!-- clean separation of HTML and PHP -->
 <p>This is the main pageeee.</p>
 <!-- backlink -->
-<?php
-// if you need the user's information, just put them into the $_SESSION variable and output them here
-echo WORDING_YOU_ARE_LOGGED_IN_AS . $_SESSION['user_name'] . "<br />";
-echo WORDING_PROFILE_PICTURE . '<br/>' . $login->user_gravatar_image_tag;
-?>
 
 
 
