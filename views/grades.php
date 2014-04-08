2eb23cd45b16fc9977781289566a58695554d03f
Robsonwp <perzfilip@gmail.com>
2014-04-08 21:24:24 +0200

    usunięcie profilowego

--- views/grades.php
+++ views/grades.php
@@ -1,13 +1,5 @@
 <?php include('_header_loggedin.php'); ?>
 
-<!-- clean separation of HTML and PHP -->
-<p>This is the main pageeee.</p>
-<!-- backlink -->
-<?php
-// if you need the user's information, just put them into the $_SESSION variable and output them here
-echo WORDING_YOU_ARE_LOGGED_IN_AS . $_SESSION['user_name'] . "<br />";
-echo WORDING_PROFILE_PICTURE . '<br/>' . $login->user_gravatar_image_tag;
-?>
 
 <div class="container">
     <div style="align:right;" class="row">
