f8853673c36f77b1a33bd94f7908a5be99924bff
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-04-13 13:58:59 +0200

    Added login check

--- tools/showGraph.php
+++ tools/showGraph.php
@@ -1,8 +1,33 @@
 <?php
+
+// check for minimum PHP version
+if (version_compare(PHP_VERSION, '5.3.7', '<')) {
+    exit('Sorry, this script does not run on a PHP version smaller than 5.3.7 !');
+} else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
+    // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
+    // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
+    require_once(dirname(__FILE__).'/../libraries/password_compatibility_library.php');
+}
+// include the config
     require dirname(__FILE__).'/../config/config.php';
+
+
+// include the PHPMailer library
+require_once(dirname(__FILE__).'/../libraries/PHPMailer.php');
     require dirname(__FILE__).'/../classes/gradeChartGenerator.php';
+// load the login class
+require_once(dirname(__FILE__).'/../classes/Login.php');
+
+// create a login object. when this object is created, it will do all login/logout stuff automatically
+// so this single line handles the entire login process.
+$login = new Login();
+
+// ... ask if we are logged in here:
+if ($login->isUserLoggedIn() == true) {
     $chartGenerator = new graphReportGenerator();
     $chartGenerator->setDatabaseConnectionData("DB_HOST2", "DB_NAME2", "DB_USER2", "DB_PASS2");
     $chartGenerator->setUserId($_SESSION['user_id']);
     $chartGenerator->executeProcessing();
-?>
+} else {
+    header('Location: login.php');
+}
