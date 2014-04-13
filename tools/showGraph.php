490c02745a370c2d4c66de61650581cee98f3357
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-04-13 13:44:47 +0200

    Added config include

--- tools/showGraph.php
+++ tools/showGraph.php
@@ -2,7 +2,7 @@
     require dirname(__FILE__).'/../config/config.php';
     require dirname(__FILE__).'/../classes/gradeChartGenerator.php';
     $chartGenerator = new graphReportGenerator();
-    $chartGenerator->setDatabaseConnectionData(DB_HOST2, DB_NAME2, DB_USER2, DB_PASS2);
+    $chartGenerator->setDatabaseConnectionData('DB_HOST2', 'DB_NAME2', 'DB_USER2', 'DB_PASS2');
     $chartGenerator->setUserId($_SESSION['user_id']);
     $chartGenerator->executeProcessing();
 ?>
